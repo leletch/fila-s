3c5a23ac21fffc54fd8ca7dbcd8bb21b8eae7a1b
lele <124884152+leletch@users.noreply.github.com>
2024-04-02 10:39:54 -0300

    Update function.js

--- pilha-fila/function.js
+++ pilha-fila/function.js
@@ -79,3 +79,158 @@ export function removeItem(item){
     // Emite um erro se a fila estiver vazia
     console.error("A fila está vazia")
 }
+
+//criarFila(size)
+
+//
+
+//removerItem()
+
+//
+
+
+
+export function createQueue(size = 10){
+
+    return [...new Array(size)]
+
+}
+
+
+
+export function addItemQueue(queue, item){
+
+    if(queue.includes(undefined)){
+
+        const index = queue.findIndex(element => element === undefined)
+
+        queue[index] = item
+
+        console.log(`"[${item}]" Foi inserido na fila, na posição -> "[${index}]"`)
+
+        console.log(queue)
+
+        return
+
+    }
+
+    
+
+    console.error("Não foi possível adicionar item, fila está cheia")
+
+    return
+
+}
+
+
+
+// export function removeItemQueue(queue){
+
+//      if(queue[0] !== undefined){
+
+//         queue[0] = undefined
+
+//         for(let i = 1; i < queue.length; i++){
+
+//              queue[i-1] = queue[i]
+
+//          }
+
+//          queue[queue.length - 1] = undefined
+
+//          return
+
+//     }
+
+//      console.error("A fila está vazia")
+
+//  }
+
+
+
+export function removeItemQueue(queue){
+
+    if(queue[0] !== undefined){
+
+        let primeiro = queue[0]
+
+       for(let i = 1; i < queue.length; i++){
+
+            queue[i-1] = queue[i]
+
+        }
+
+        queue[queue.length - 1] = undefined
+
+        return
+
+   }
+
+    console.error("A fila está vazia")
+
+           }
+
+                  1-//verificar se a fila está vazia
+
+export function estaVazia(fila){
+If (fila[0]===undefined){
+console.log(“está vazia”)
+return 
+}
+
+
+2-//aumentar o tamanho da fila 
+Não sei
+
+3-//diminuir o tamanho da fila 
+Não sei
+
+4-//verificar o tamanho da fila 
+ export function verificarTamanho(fila){
+return fila.lenght 
+}
+
+
+5-// limpar a fila
+export function limparFila(fila){
+Fila.forEach (item =>(item=undefined))
+}
+
+6-// verificar o primeiro elemento da fila 
+export function primeiroElemento (fila){
+         filaVazia(fila);
+    return fila[0]
+}
+
+
+7-// verificar o último elemento da fila
+
+export function verificarUltimo(fila) {
+    filaVazia(fila);
+    return
+ fila[fila.indexOf(undefined) - 1];
+}
+
+8- //verificar se um elemento está na fila
+
+export function elementoFila(elemento,fila){
+return fila.includes(elementos)
+}
+
+9-//verificar a posição de um elemento na fila
+export function posicaoElemento (fila,elemento ){
+If (fila.indexOf (elemento )=== -1){
+console.log(“fila não possui esse elemento ”)
+return 
+}
+return fila.indexOf(elemento )
+}
+
+10-//verificar se a fila está cheia
+
+export function filaCheia(fila){
+If (fila[fila.lenght-1]!=undefined){
+console.log(“fila cheia”)
+return 
+}
+}
